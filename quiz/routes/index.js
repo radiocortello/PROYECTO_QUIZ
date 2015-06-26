a754571114cc185feacfbf13fcc43dbaed26f7ce
radiocortello <radiocortello@hotmail.com>
2015-06-26 18:01:46 +0200

    modulo 6
    cambios necesarios para completar el módulo 6

--- quiz/routes/index.js
+++ quiz/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+var quiz_controller = require('../controllers/quiz_controller')
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { layout:false, titulo: 'QUIZ' });
 });
-
+router.get('/quizes/question', quiz_controller.question);
+router.get('/quizes/answer', quiz_controller.answer);
+router.get('/author', function (req,res,next) {
+  res.render('author')
+});
 module.exports = router;
