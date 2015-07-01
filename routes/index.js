2eee3a62e3fa41e59da6f39849833d3a225e7bd0
radiocortello <radiocortello@hotmail.com>
2015-07-01 21:29:25 +0200

    modulo 7 post preguntas

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,9 @@ router.param('quizId', quiz_controller.load); //autoload :quizId
 router.get('/quizes/', quiz_controller.index);
 router.get('/quizes/:quizId(\\d+)', quiz_controller.show)
 router.get('/quizes/:quizId(\\d+)/answer', quiz_controller.answer);
+router.get('/quizes/new', quiz_controller.new)
 router.get('/author', function (req,res,next) {
   res.render('author')
 });
+router.post('/quizes/create', quiz_controller.create);
 module.exports = router;
